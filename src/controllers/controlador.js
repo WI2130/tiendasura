2782e47a71af1952aac06212e758edad62678864
fandresh62183 <fandresh62183@gmail.com>
2022-07-21 17:43:01 -0500

    se modifica y se crea controlador

---
+++ src/controllers/controlador.js
@@ -0,0 +1,10 @@
+// creando variables 
+
+let nombreUsuario="john vargas"
+const EDAD_USUARIO=35
+let estaturaUsuario=1.70
+let hinvhaDelVerde=true
+
+console.log(nombreUsuario)
+
+console.log(EDAD_USUARIO)
